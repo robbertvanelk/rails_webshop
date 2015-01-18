d0f0489956266e91bf5d86c8217529580e0dc180
robbertvanelk <robbert_vanelk@hotmail.com>
2015-01-18 17:49:57 +0100

    Gui Update

--- shop/app/controllers/home_controller.rb
+++ shop/app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   def index
   	@category = Category.all
   	@products = Product.order("products.count DESC").limit(3)
+  	@productsnew = Product.order("products.created_at DESC").limit(3)
   end
 end
